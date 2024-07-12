3fe69c855d4a2bd8d5466e073e8ba53c86f08abd
Volker Kempert <volker.kempert@almedso.de>
2024-07-12 18:52:33 +0200

    Adjust layout
    - outer most keys are not used anymore (on both sides)
    - shift ctrl gui alt is consequently assigned to all relevant layers

--- src/layout.rs
+++ src/layout.rs
@@ -6,34 +6,6 @@ type Action = keyberon::action::Action<()>;
 static EXTRALAYER: Action = Action::DefaultLayer(3);
 static QWERTZLAYER: Action = Action::DefaultLayer(0);
 
-const TIMEOUT: u16 = 200;
-
-
-const CTRL_SP: Action = HoldTap(&HoldTapAction {
-    timeout: TIMEOUT,
-    tap_hold_interval: 200,
-    config: HoldTapConfig::Default,
-    hold: k(LCtrl),
-    tap: k(Space),
-});
-
-const ALT_ENT: Action = HoldTap(&HoldTapAction {
-    timeout: TIMEOUT,
-    tap_hold_interval: 200,
-    config: HoldTapConfig::Default,
-    hold: k(LAlt),
-    tap: k(Enter),
-});
-
-const PPN: Action = HoldTap(&HoldTapAction {
-    timeout: TIMEOUT,
-    tap_hold_interval: 200,
-    config: HoldTapConfig::Default,
-    hold: k(MediaNextSong),
-    tap: k(MediaPlayPause),
-});
-
-
 macro_rules! s {
     ($k:ident) => {
         m(&[LShift, $k].as_slice())
@@ -45,23 +17,176 @@ macro_rules! a {
     };
 }
 
+const TIMEOUT: u16 = 200;
+
+const PPN: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(MediaNextSong),
+    tap: k(MediaPlayPause),
+});
+
+// Level 0 - hold and tap
+
+const A_SH: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LShift),
+    tap: k(A),
+});
+
+const S_CTL: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LCtrl),
+    tap: k(S),
+});
+
+const D_GUI: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LGui),
+    tap: k(D),
+});
+
+const F_ALT: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LAlt),
+    tap: k(F),
+});
+
+const OE_SH: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(RShift),
+    tap: k(SColon), // Ö
+});
+
+const L_CTL: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(RCtrl),
+    tap: k(L),
+});
+
+const K_GUI: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LGui),
+    tap: k(K),
+});
+
+const KB5_GUI: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LGui),
+    tap: k(Kb5),
+});
+
+const J_ALT: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LAlt),
+    tap: k(J),
+});
+
+// Level 1 - hold and tap
+
+const BS_CTL: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LCtrl),
+    tap: s!(Kb7),
+});
+
+const CCB_ALT: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LAlt),
+    tap: s!(Kb0),
+});
+
+const MI_CTL: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LCtrl),
+    tap: k(Slash),
+});
+
+const OB_ALT: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LAlt),
+    tap: s!(Kb8),
+});
+
+// Level 2 - hold and tap
+
+const HOME_SH: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LShift),
+    tap: k(Home),
+});
+
+const LEFT_CTL: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LCtrl),
+    tap: k(Left),
+});
+
+const DOWN_GUI: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LGui),
+    tap: k(Down),
+});
+
+const RIGHT_ALT: Action = HoldTap(&HoldTapAction {
+    timeout: TIMEOUT,
+    tap_hold_interval: 200,
+    config: HoldTapConfig::Default,
+    hold: k(LAlt),
+    tap: k(Right),
+});
+
+
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers<12, 4, 4, ()> = keyberon::layout::layout! {
     {
-        [ Tab    Q   W   E   R   T     Y   U   I   O   P      LBracket]
-        [ Escape A   S   D   F   G     H   J   K   L   SColon Quote   ]
-        [ LShift Z   X   C   V   B     N   M   ,   .   Slash  RShift  ]
-        [ n n n LCtrl {ALT_ENT} (2)   (1) {CTRL_SP} LAlt n n  n           ]
+        [ Tab    Q      W       E       R       T     Y   U       I       O       P       n      ]
+        [ Escape {A_SH} {S_CTL} {D_GUI} {F_ALT} G     H   {J_ALT} {K_GUI} {L_CTL} {OE_SH} n      ]
+        [ LShift Z      X       C       V       B     N   M       ,       .       Slash   RShift ]
+        [                 n n n Escape  Enter  (2)   (1)  Space   Tab n n n                      ]
     }{
-        [ t {a!(E)}     {s!(Grave)} {a!(Kb8)}         {a!(Kb9)}      Grave          {s!(Kb1)}   NonUsBslash {s!(NonUsBslash)} {s!(Kb0)}       {s!(Kb6)}   BSpace  ],
-        [ t {a!(Minus)} {s!(Kb7)}   {a!(Kb7)}         {a!(Kb0)}      {s!(RBracket)} {s!(Minus)} {s!(Kb8)}   {s!(Kb9)}         Slash           {s!(Dot)}   {a!(Q)} ],
-        [ t NonUsHash   {s!(Kb4)}   {a!(NonUsBslash)} {a!(RBracket)} {s!(Equal)}    RBracket    {s!(Kb5)}   {s!(Kb2)}         {s!(NonUsHash)} {s!(Comma)} t       ],
-        [ t t           t           t                 t              t              {EXTRALAYER} t          t                 t               t           t       ],
+        [ t {a!(E)}     {s!(Grave)} {a!(Kb8)}         {a!(Kb9)}      Grave          {s!(Kb1)}   NonUsBslash {s!(NonUsBslash)} {s!(Kb0)}       {s!(Kb6)}   t ],
+        [ t {a!(Minus)} {BS_CTL}    {a!(Kb7)}         {CCB_ALT}      {s!(RBracket)} {s!(Minus)} {OB_ALT}    {s!(Kb9)}         {MI_CTL}        {a!(Q)}     t ],
+        [ t NonUsHash   {s!(Kb4)}   {a!(NonUsBslash)} {a!(RBracket)} {s!(Equal)}    RBracket    {s!(Kb5)}   {s!(Kb2)}         {s!(NonUsHash)} {s!(Comma)} t ],
+        [ t t           t           t                 t              t              {EXTRALAYER} t          t                 t               t           t ],
     }{
-        [ t  PgUp   Delete  Up  BSpace PgDown         -        Kb7 Kb8 Kb9 Minus BSpace ],
-        [ t  Home   Left   Down Right  End            Kb0      Kb4 Kb5 Kb6 Comma Dot    ],
-        [ t  LGui   Tab    Mute Enter  LGui           RBracket Kb1 Kb2 Kb3 LGui  t      ],
-        [ t  t      t      t    t      {EXTRALAYER}   t        t   t   t   t     t      ],
+        [ t  PgUp      Delete     Up         BSpace      PgDown         -        Kb7 Kb8       Kb9 Minus    t ], // Minsus = ß
+        [ t  {HOME_SH} {LEFT_CTL} {DOWN_GUI} {RIGHT_ALT} End            Kb0      Kb4 {KB5_GUI} Kb6 LBracket t ], // LBracket = Ü
+        [ t  LGui      Tab        Mute       Enter       CapsLock       RBracket Kb1 Kb2       Kb3 Quote    t ], // Quote = Ä ; RBracket = +
+        [ t  t         t          t          t           {EXTRALAYER}   t        t   t         t   t        t ],
     }{
         [{Custom(())}  n    n     {PPN} VolUp    n             PScreen    F7  F8  F9  F10  {Custom(())}],
         [{QWERTZLAYER} n    n     n     Mute     n            ScrollLock F4  F5  F6   F11  t           ],
